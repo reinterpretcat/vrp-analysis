32cdc82889470743476a08f873bd426f4145489b
reinterpretcat <ilya.builuk@gmail.com>
2020-07-11 00:39:29 +0200

    Improve tour statistics

--- notebooks/analyze_solution.py
+++ notebooks/analyze_solution.py
@@ -24,8 +24,19 @@ def extract_tours_statistic(solution):
     """
     Extracts statistic data for all tours
     """
-    df = pd.json_normalize(solution['tours']).drop('stops', axis=1)  
+    def count_activity_types(stops, activity_type):
+        return sum([1 if a['type'] == activity_type else 0 for stop in stops for a in stop['activities']])
+
+    df = pd.json_normalize(solution['tours'])
     df.columns = df.columns.str.replace('statistic.', '')
     df.columns = df.columns.str.replace('times.', '')
+
+    df['activities'] = df.apply(lambda row: sum(map(lambda stop: len(stop['activities']), row.stops)), axis=1)
+    df['deliveries'] = df.apply(lambda row: count_activity_types(row.stops, 'delivery'), axis=1)
+    df['pickups'] = df.apply(lambda row: count_activity_types(row.stops, 'pickup'), axis=1)
+    df['breaks'] = df.apply(lambda row: count_activity_types(row.stops, 'break'), axis=1)
+    df['reloads'] = df.apply(lambda row: count_activity_types(row.stops, 'reload'), axis=1)
+
+    df['stops'] = df.apply(lambda row: len(row.stops), axis=1)
 
     return df
